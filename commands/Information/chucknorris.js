0479548c6eede0422fdd750c02dd1b9c66dc1b01
Michael (aka. Leberkassemmel) <michi.11880+leberkasschnitzelgithub@gmail.com>
2018-08-19 01:39:52 +0200

    Added !chucknorris for some chuck norris quotes

---
+++ commands/Information/chucknorris.js
@@ -0,0 +1,63 @@
+const { Command } = require('klasa');
+const { MessageEmbed } = require('discord.js');
+const fetch = require('node-fetch');
+
+module.exports = class extends Command {
+
+    constructor(...args) {
+        /**
+         * Any default options can be omitted completely.
+         * if all options are default, you can omit the constructor completely
+         */
+        super(...args, {
+            enabled: true,
+            runIn: ['text', 'dm', 'group'],
+            requiredPermissions: [],
+            requiredSettings: [],
+            aliases: [],
+            autoAliases: true,
+            bucket: 1,
+            cooldown: 5,
+            promptLimit: 0,
+            promptTime: 30000,
+            deletable: false,
+            guarded: false,
+            nsfw: false,
+            permissionLevel: 0,
+            description: 'Random facts about Chuck norris. Provided by api.chucknorris.io',
+            extendedHelp: 'Random facts about Chuck Norris. Provided by api.chucknorris.io',
+            usage: '',
+            usageDelim: undefined,
+            quotedStringSupport: false,
+            subcommands: false
+        });
+    }
+
+    async run(message, [...params]) {
+        try {
+            let data = await fetch(`https://api.chucknorris.io/jokes/random`);
+            let response = JSON.parse(await data.text());
+            let embed = new MessageEmbed()
+            .setTimestamp()
+            .setColor('#dd67ff')
+            .addField('Chuck Norris', response.value)
+            .setFooter(`Requested by: ${message.author.tag} | Provided by api.chucknorris.io`)
+            ;
+            message.send(embed);
+        } catch (error) {
+            console.log(error);
+            let embed = new MessageEmbed();
+            embed.setTitle('Something went wrong!');
+            embed.setColor('red');
+            message.send(embed);
+        }
+    }
+
+    async init() {
+        /*
+         * You can optionally define this method which will be run when the bot starts
+         * (after login, so discord data is available via this.client)
+         */
+    }
+
+};
