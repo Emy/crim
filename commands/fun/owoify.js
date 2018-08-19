3e02b7d6c446dc3361195c96fe4363cbb5de3cba
Marc Desch <me@marcdesch.de>
2018-08-19 19:08:56 +0200

    improve owoify

--- commands/fun/owoify.js
+++ commands/fun/owoify.js
@@ -30,25 +30,16 @@ module.exports = class extends Command {
     }
 
     async run(message, [text]) {
-        try {
-            let data = await fetch(`https://nekos.life/api/v2/owoify?text=${encodeURIComponent(text)}`);
-            let response = JSON.parse(await data.text());
-            let embed = new MessageEmbed()
-            .setColor('#dd67ff')
-            .addField('OwOified', response.owo)
-            .addField('Original', text)
-            .setFooter(`Requested by: ${message.author.tag}`)
-            .setTimestamp()
-            ;
-            message.send(embed);
-            if (message.deletable) await message.delete();
-        } catch (error) {
-            console.log(error);
-            let embed = new MessageEmbed();
-            embed.setTitle('Something went wrong!');
-            embed.setColor('red');
-            message.send(embed);
-        }
+        let owoified = text.replace(/r|l/g, "w").replace(/R|L/g, "W").replace(/owo/i, 'OwO').replace(/uwu/i, 'UwU');
+        let embed = new MessageEmbed()
+        .setColor('#dd67ff')
+        .addField('OwOified', owoified)
+        .addField('Original', text)
+        .setFooter(`Requested by: ${message.author.tag}`)
+        .setTimestamp()
+        ;
+        message.send(embed);
+        if (message.deletable) await message.delete();
     }
 
     async init() {
