716e826a8258ea8cd1a5c8534ee0e2812d359b66
Marc Desch <me@marcdesch.de>
2018-09-06 17:53:54 +0200

    add slap command

---
+++ commands/fun/slap.js
@@ -0,0 +1,55 @@
+const { Command } = require('klasa');
+const { MessageEmbed } = require('discord.js');
+const fetch = require('node-fetch');
+
+module.exports = class extends Command {
+
+    constructor(...args) {
+        super(...args, {
+            enabled: true,
+            runIn: ['text', 'group'],
+            requiredPermissions: [],
+            requiredSettings: [],
+            aliases: [],
+            autoAliases: true,
+            bucket: 1,
+            cooldown: 5,
+            promptLimit: 0,
+            promptTime: 30000,
+            deletable: false,
+            guarded: false,
+            nsfw: false,
+            permissionLevel: 0,
+            description: 'Slap people.',
+            extendedHelp: 'No extended help available.',
+            usage: '<member:member>',
+            usageDelim: undefined,
+            quotedStringSupport: false,
+            subcommands: false
+        });
+    }
+
+    async run(message, [member]) {
+        try {
+            let data = await fetch('https://nekos.life/api/v2/img/slap');
+            let response = JSON.parse(await data.text());
+            let embed = new MessageEmbed()
+            .setTitle(`**${message.author.username}** is slapping **${member.user.username}** >.<`)
+            .setColor('#dd67ff')
+            .setFooter(`Requested by ${message.author.tag} | Provided by nekos.life`)
+            .setTimestamp()
+            .setImage(response.url)
+            ;
+            message.send(embed);
+        } catch (error) {
+            console.log(error);
+            let embed = new MessageEmbed();
+            embed.setTitle('Something went wrong!');
+            embed.setColor('red');
+            message.send(embed);
+        }
+    }
+
+    async init() {}
+
+};
