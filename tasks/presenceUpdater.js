c8af3af371a1223dc51f2c364f6512c458fafa88
Emy <me@emy.moe>
2019-05-03 13:15:52 +0200

    replaced old news with invite command

--- tasks/presenceUpdater.js
+++ tasks/presenceUpdater.js
@@ -12,11 +12,10 @@ module.exports = class extends Task {
             `with ${this.client.users.size} users.`,
             `on ${this.client.guilds.size} servers.`,
             `@${this.client.user.username} help`,
-            `Emybot now is Filo!`,
+            `@${this.client.user.username} invite`,
         ]
         this.currentPosition = this.currentPosition % presenceRotation.length;
         this.client.user.setPresence( { activity: {name: presenceRotation[this.currentPosition++]}, status: 'online'} );
     }
-
 
 };
