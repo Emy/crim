380fcd1c4ad5d136362ebd9a911ef0701fa94fa8
Emy <me@emy.moe>
2019-10-02 22:41:00 +0200

    reworking the fun category

--- commands/General/Fun/kiss.js
+++ commands/General/Fun/kiss.js
@@ -7,27 +7,24 @@ module.exports = class extends Command {
       runIn: ['text', 'group'],
       requiredPermissions: ['EMBED_LINKS'],
       cooldown: 5,
-      description: (language) => language.get('COMMAND_KISS_DESCRIPTION'),
+      description: (lang) => lang.get('KISS_DESCRIPTION'),
       usage: '<member:member>',
     });
   }
 
-  async run(message, [member]) {
-    let data;
-    const author = message.author;
-    const user = member.user;
+  async run(msg, [member]) {
     try {
-      const response = await fetch('https://nekos.life/api/v2/img/kiss');
-      data = await response.json();
+      const author = msg.author;
+      const user = member.user;
+      const data = await (await fetch('https://nekos.life/api/v2/img/kiss')).json();
+      if (!(data || data.url)) return msg.sendError('NO_DATA');
+      msg.genEmbed()
+          .setEmoteTitle(author.username, user.username, 'KISSING', true)
+          .setProvidedBy('nekos.life')
+          .setImage(data.url)
+          .send();
     } catch (error) {
-      return message.sendError('ERROR_REST_REQUEST_FAILED');
+      return msg.sendError('REQUEST_FAILED');
     }
-
-    if (!(data || data.url)) return message.sendError('ERROR_REST_NO_DATA');
-    message.genEmbed()
-        .setEmoteTitle(author.username, user.username, 'EMOTE_KISS', true)
-        .setProvidedBy('nekos.life')
-        .setImage(data.url)
-        .send();
   }
 };
