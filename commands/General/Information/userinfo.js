1ce68bfc14e9bf2adc5e99c5478a117949d59e2e
Emy <me@emy.moe>
2020-02-23 18:57:17 +0100

    :truck: moving whois to userinfo

--- commands/General/Information/userinfo.js
+++ commands/General/Information/userinfo.js
@@ -7,24 +7,25 @@ module.exports = class extends Command {
       enabled: false,
       runIn: ['text'],
       requiredPermissions: ['EMBED_LINKS'],
-      aliases: ['info'],
+      aliases: ['info', 'uinfo', 'whois'],
       cooldown: 5,
-      description: (lang) => lang.get('WHOIS_DESCRIPTION'),
+      description: (lang) => lang.get('USERINFO_DESCRIPTION'),
       usage: '<member:member>',
     });
   }
 
   async run(msg, [member]) {
+    const lang = msg.language;
     const emojis = this.client.emojis.cache;
     let roles = '';
-    member.roles.map((r) => r.name != '@everyone' ? roles += `${r.name} ` : '');
+    member.roles.cache.map((r) => r.name != '@everyone' ? roles += `${r.name} ` : '');
     msg.genEmbed()
         .setTitle(`${emojis.get(emoji.id)} ${member.user.username}`)
         .setThumbnail(member.user.avatarURL())
         .addField(lang.get('NAME'), member.user.tag, true)
         .addField(lang.get('USER_ID'), member.user.id, true)
         .addField(lang.get('CREATED_AT'), member.user.createdAt, true)
-        .addField(lang.get('ROLES'), roles)
+        .addField(lang.get('ROLES'), roles == '' ? 'No roles.': roles)
         .send();
   }
 };
